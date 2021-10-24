b9bfc38b371872d4f1582f89028899fb3ec17ddc
Darya Anishchenko <Darya.Anishchenko@bonava.com>
2021-10-24 17:51:50 +0300

    Добавьте файлы проекта.

---
+++ Braces/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Braces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку со скобками");
+            string task = Console.ReadLine();
+
+            Stack<char> reverseBracesStack = new Stack<char>();
+            char compareBraces;
+            bool rightBraces = true;
+
+            foreach (char symbol in task)
+            {
+                switch (symbol)
+                {
+                    //Делаем обратные скобки в stack
+                    case '{':
+                        reverseBracesStack.Push('}');
+                        break;
+                    case '(':
+                        reverseBracesStack.Push(')');
+                        break;
+                    case '[':
+                        reverseBracesStack.Push(']');
+                        break;
+
+                    //Сравниваем обратные скобки с последними добавленными в stack
+                    case '}':
+                        compareBraces = reverseBracesStack.Pop();
+                        if (symbol != compareBraces)
+                        {
+                            rightBraces = false;
+                        }
+                        break;
+                    case ')':
+                        compareBraces = reverseBracesStack.Pop();
+                        if (symbol != compareBraces)
+                        {
+                            rightBraces = false;
+                        }
+                        break;
+                    case ']':
+                        compareBraces = reverseBracesStack.Pop();
+                        if (symbol != compareBraces)
+                        {
+                            rightBraces = false;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (rightBraces)
+            {
+                Console.WriteLine("Скобки расставлены верно.");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены неверно.");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
